a301ac2ec5dddb2ddc09c013bc03c372a09bfd6d
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-05-12 20:29:04 -0300

    (feat): CRUD operations for categories

--- app/stock/crud.py
+++ app/stock/crud.py
@@ -1,2 +1,43 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.encoders import jsonable_encoder
 
+
+def crud_factory(model):
+    class Crud:
+        @classmethod
+        def get_all(cls, db):
+            return jsonable_encoder(db.query(model).all())
+
+        @classmethod
+        def get_by_name(cls, db, name):
+            return jsonable_encoder(db.query(model).filter_by(name=name).first())
+
+        @classmethod
+        def get_by_id(cls, db, id):
+            return jsonable_encoder(db.query(model).get(id))
+
+        @classmethod
+        def create(cls, db, data):
+            instance = model(**data.model_dump())
+            db.add(instance)
+            db.commit()
+            db.refresh(instance)
+            return jsonable_encoder(instance)
+
+        @classmethod
+        def update(cls, db, id, data):
+            instance = db.query(model).get(id)
+            for key, value in data.dict().items():
+                setattr(instance, key, value)
+            db.commit()
+            db.refresh(instance)
+            return jsonable_encoder(instance)
+
+        @classmethod
+        def delete(cls, db, id):
+            instance = db.query(model).get(id)
+            db.delete(instance)
+            db.commit()
+            return jsonable_encoder(instance)
+
+    Crud.model = model
+    return Crud
--- app/stock/routers/category.py
+++ app/stock/routers/category.py
@@ -1,22 +1,37 @@
 from fastapi import APIRouter, Depends
 from config.database import get_db
-from stock.models import Category as CategoryModel
+
 from stock.schemas import CategoryCreate, CategoryUpdate, Category
 from config.database import Base, engine
+from stock.controllers.category import CategoryController
+
+from stock.crud import crud_factory
 
 router = APIRouter()
 
 Base.metadata.create_all(bind=engine)
 
+
 @router.get('/', response_model=list[Category])
 async def get_categories(db=Depends(get_db)):
-    return db.query(CategoryModel).all()
+    return CategoryController(db).get_all()
 
 
 @router.post('/', response_model=Category)
 async def create_category(category: CategoryCreate, db=Depends(get_db)):
-    category = CategoryModel(**category.model_dump())
-    db.add(category)
-    db.commit()
-    db.refresh(category)
-    return category
+    return CategoryController(db).create(category)
+
+
+@router.get('/{id}', response_model=Category)
+async def get_category(id: int, db=Depends(get_db)):
+    return CategoryController(db).get_by_id(id)
+
+
+@router.put('/{id}', response_model=Category)
+async def update_category(id: int, category: CategoryUpdate, db=Depends(get_db)):
+    return CategoryController(db).update(id, category)
+
+
+@router.delete('/{id}', response_model=Category)
+async def delete_category(id: int, db=Depends(get_db)):
+    return CategoryController(db).delete(id)
