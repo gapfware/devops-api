d7fc8b8192069d91dd77c7af67304762ff6cb839
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-05-12 21:45:49 -0300

    (feat): Add error handler and CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.category import router as category_router
-
+from middleware.error_handler import ErrorHandler
 
 
 app = FastAPI()
 
-app.include_router(category_router, prefix='/categories', tags=['categories'])
+app.add_middleware(ErrorHandler)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
+app.include_router(category_router, prefix='/categories', tags=['categories'])
 
 
 @app.get("/")
