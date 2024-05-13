d6b447ddc5125608b62176aaa422932502603709
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-05-12 21:53:28 -0300

    (change): Root redirects to home

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.category import router as category_router
 from middleware.error_handler import ErrorHandler
+from fastapi.responses import RedirectResponse
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Devops API",
+    version="0.1",
+)
 
 app.add_middleware(ErrorHandler)
 app.add_middleware(
@@ -20,4 +24,4 @@ app.include_router(category_router, prefix='/categories', tags=['categories'])
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return RedirectResponse(url="/docs")
