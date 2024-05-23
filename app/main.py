6d1540f64421d486b8c65db80dfb2141557462c1
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-05-23 17:42:03 -0300

    (feat): Generate Docstring

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from app.routers.category import router as category_router
+from app.routers.product import router as product_router
 from app.middleware.error_handler import ErrorHandler
 from app.config.database import engine, Base
-from fastapi.responses import RedirectResponse
-from app.routers.product import router as product_router
-
 
 app = FastAPI(
     title="Devops API",
     version="0.1",
 )
+
 Base.metadata.create_all(bind=engine)
 
 app.add_middleware(ErrorHandler)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -22,11 +23,16 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+app.include_router(
+    category_router, prefix='/api/v1/categories', tags=['categories']
+)
 
-app.include_router(category_router, prefix='/categories', tags=['categories'])
-app.include_router(product_router, prefix='/products', tags=['products'])
+app.include_router(
+    product_router, prefix='/api/v1/products', tags=['products']
+)
 
 
 @app.get("/")
 def read_root():
+    """Redirects to the API documentation."""
     return RedirectResponse(url="/docs")
--- app/routers/category.py
+++ app/routers/category.py
@@ -9,24 +9,79 @@ router = APIRouter()
 
 @router.get('/', response_model=list[Category])
 async def get_categories(db=Depends(get_db)):
+    """
+    Retrieve all categories from the database.
+
+    Parameters:
+    - db: The database dependency.
+
+    Returns:
+    - A list of Category objects representing all categories in the database.
+    """
     return CategoryController(db).get_all()
 
 
 @router.post('/', response_model=Category)
 async def create_category(category: CategoryCreate, db=Depends(get_db)):
+    """
+    Create a new category.
+
+    Args:
+        category (CategoryCreate): The category data to be created.
+        db: The database dependency.
+
+    Returns:
+        Category: The created category.
+    """
     return CategoryController(db).create(category)
 
 
 @router.get('/{id}', response_model=Category)
 async def get_category(id: int, db=Depends(get_db)):
+    """
+    Retrieve a category by its ID.
+
+    Parameters:
+    - id (int): The ID of the category to retrieve.
+    - db (Database): The database connection.
+
+    Returns:
+    - Category: The retrieved category.
+
+    Raises:
+    - HTTPException: If the category with the given ID does not exist.
+    """
     return CategoryController(db).get_by_id(id)
 
 
 @router.put('/{id}', response_model=Category)
 async def update_category(id: int, category: CategoryUpdate, db=Depends(get_db)):
+    """
+    Update a category with the given ID.
+
+    Args:
+        id (int): The ID of the category to update.
+        category (CategoryUpdate): The updated category data.
+        db: The database dependency.
+
+    Returns:
+        Category: The updated category.
+
+    """
     return CategoryController(db).update(id, category)
 
 
 @router.delete('/{id}', response_model=Category)
 async def delete_category(id: int, db=Depends(get_db)):
+    """
+    Delete a category by its ID.
+
+    Parameters:
+    - id (int): The ID of the category to be deleted.
+    - db: The database dependency.
+
+    Returns:
+    - Category: The deleted category.
+
+    """
     return CategoryController(db).delete(id)
