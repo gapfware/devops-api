50a5aeed5db47d812d9cf3b442c0996a15efea9c
Gabriel Perez Federico <perezgaa@globalhitss.com>
2024-05-24 17:54:16 -0300

    (feat): Create db for env

--- app/config/database.py
+++ app/config/database.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
 
 load_dotenv()
 
@@ -11,9 +12,12 @@ password = getenv("POSTGRES_PASSWORD")
 host = getenv("POSTGRES_HOST")
 port = getenv("POSTGRES_PORT")
 database = getenv("POSTGRES_DB")
+environment = getenv("ENV")
 
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{user}:{password}@{host}:{port}/{database}'  # Modify this line
+if environment == "development":
+    SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
+else:
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
